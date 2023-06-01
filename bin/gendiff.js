05f19dba17c3cc0f40387e87db7354c25f85b431
gaivanchi <37071300+gaivanchi@users.noreply.github.com>
2023-06-01 14:25:30 +0500

    Add func gendiff with basic functionality

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-// import genDiff from '../src/index.js';
+import path from 'path';
+import genDiff from '../lib/index.js';
+
+const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const program = new Command();  
 
@@ -10,6 +13,12 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
-  
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, options) => {
+    const fullPath1 = buildFullPath(filepath1);
+    const fullPath2 = buildFullPath(filepath2);
+
+    console.log(genDiff(fullPath1, fullPath2, options.format));
+  });
+
 program.parse();
