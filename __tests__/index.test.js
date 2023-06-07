3b8f61188747035f66c286a8f4bd31174c53d5a1
gaivanchi <37071300+gaivanchi@users.noreply.github.com>
2023-06-07 12:42:07 +0500

    Add tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,20 @@
+import path, { dirname } from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import genDiff from '../lib/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
+
+test('gendiff for flat files', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+
+  const diff = genDiff(filepath1, filepath2);
+  const result = readFile('result.txt');
+
+  expect(diff).toBe(result);
+});
