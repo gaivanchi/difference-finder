18efcfe59f5f139ce7ae59c744a53b3bdd7d1e36
gaivanchi <37071300+gaivanchi@users.noreply.github.com>
2023-07-04 13:59:51 +0500

    Update bin/gendiff.js (add default option value)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+
 import genDiff from '../lib/index.js';
 
 const program = new Command();
@@ -10,7 +11,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => console
     .log(genDiff(filepath1, filepath2, options.format)));
 
