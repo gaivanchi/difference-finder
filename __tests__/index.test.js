ed1f2a21a70057e717f77d820964defa93fdd526
gaivanchi <37071300+gaivanchi@users.noreply.github.com>
2023-07-04 13:55:06 +0500

    Update tests (support nested files)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-test('gendiff with flat JSON files', () => {
+test('gendiff with JSON files', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
 
@@ -19,7 +19,7 @@ test('gendiff with flat JSON files', () => {
   expect(diff).toBe(result);
 });
 
-test('gendiff with flat YAML files', () => {
+test('gendiff with YAML files', () => {
   const filepath1 = getFixturePath('file1.yml');
   const filepath2 = getFixturePath('file2.yaml');
 
