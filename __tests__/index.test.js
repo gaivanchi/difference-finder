ef71d5b8605435ec9be7c0c20b5a440edee6d4c5
gaivanchi <37071300+gaivanchi@users.noreply.github.com>
2023-07-05 22:51:26 +0500

    Update tests (support plain output format)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,22 +9,42 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-test('gendiff with JSON files', () => {
+test('gendiff with JSON files (default "stylish" format)', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
 
   const diff = genDiff(filepath1, filepath2);
-  const result = readFile('result.txt');
+  const result = readFile('resultStylish.txt');
 
   expect(diff).toBe(result);
 });
 
-test('gendiff with YAML files', () => {
+test('gendiff with YAML files (default "stylish" format)', () => {
   const filepath1 = getFixturePath('file1.yml');
   const filepath2 = getFixturePath('file2.yaml');
 
   const diff = genDiff(filepath1, filepath2);
-  const result = readFile('result.txt');
+  const result = readFile('resultStylish.txt');
+
+  expect(diff).toBe(result);
+});
+
+test('gendiff with JSON files ("plain" format)', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+
+  const diff = genDiff(filepath1, filepath2, 'plain');
+  const result = readFile('resultPlain.txt');
+
+  expect(diff).toBe(result);
+});
+
+test('gendiff with YAML files ("plain" format)', () => {
+  const filepath1 = getFixturePath('file1.yml');
+  const filepath2 = getFixturePath('file2.yaml');
+
+  const diff = genDiff(filepath1, filepath2, 'plain');
+  const result = readFile('resultPlain.txt');
 
   expect(diff).toBe(result);
 });
